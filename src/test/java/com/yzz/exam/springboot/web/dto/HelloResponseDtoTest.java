f4b354cbf58d76feaf332acce21ba1d5f93e1179
jeon_yejin <yemong1226@gmaill.com>
2020-11-24 20:02:15 +0900

    JPA 추가

--- src/test/java/com/yzz/exam/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/yzz/exam/springboot/web/dto/HelloResponseDtoTest.java
@@ -3,7 +3,7 @@ package com.yzz.exam.springboot.web.dto;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+/* gradle 4 버전 필요. 5 버전부터 설정 바뀌어서 컴파일 안됨. */
 public class HelloResponseDtoTest {
     @Test
     public void 롬복_기능_테스트() {
