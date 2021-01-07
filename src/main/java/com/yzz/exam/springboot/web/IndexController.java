58affa42f04cdbb4bfa159fc9faad8b37a361fcf
jeon_yejin <yemong1226@gmaill.com>
2021-01-07 16:29:39 +0900

    커밋

--- src/main/java/com/yzz/exam/springboot/web/IndexController.java
+++ src/main/java/com/yzz/exam/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.yzz.exam.springboot.web;
 
+import com.yzz.exam.springboot.config.auth.dto.SessionUser;
+import com.yzz.exam.springboot.domain.user.User;
 import com.yzz.exam.springboot.service.posts.PostsService;
 import com.yzz.exam.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +10,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
